22e91fda161951a8bf87378aedadb18f3d2bb9b3
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2021-04-01 08:50:56 -0300

    initial components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Home } from './Home/Home';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hi</h1>
+
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
