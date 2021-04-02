f4edaa72ec1efd21aca9b1fc8746e8b2ea31deb3
Fabio Hiroki <fabiothiroki@gmail.com>
2021-04-02 10:37:05 -0300

    Add icon (#2)
    * Add search icon
    
    * header style

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import LanguageIcon from '@material-ui/icons/Language';
+import { Avatar } from '@material-ui/core';
 
 export const Header = () => {
   return (
@@ -9,6 +13,18 @@ export const Header = () => {
           src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
           alt=""
       />
+
+      <div className='header__center'>
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className='header__right'>
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
     </div>
   );
 };
